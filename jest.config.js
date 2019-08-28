e45809e1ab0dc6b54b0eb54d8d45c9a83bfd9403
Iveren Shaguy <iverenshaguy@gmail.com>
2019-08-28 01:36:12 +0100

    chore(ci): setup circle ci
    - update implementation
    
    [Starts #167377486]

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  reporters: ["default", ["jest-junit", { suiteName: "jest tests" }]],
+  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
+  testMatch: [
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx}'
+  ],
   collectCoverage: true
 };
